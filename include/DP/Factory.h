eb24be865c8d94b9f9adb38f27eaef8a479dbf7a
ahfakt <a.farukakts@outlook.com>
2022-07-26 17:30:55 +0300

    ...

--- include/DP/Factory.h
+++ include/DP/Factory.h
@@ -1,54 +1,43 @@
 #ifndef DP_FACTORY_H
 #define DP_FACTORY_H
 
-#include "DP/CreateInfo.h"
+#include "CreateInfo.h"
 #include <stdexcept>
 #include <unordered_map>
 
 namespace DP {
 
-template <typename T, typename IDType, typename ... Args>
+template <typename T, typename IdType, typename ... Args>
 class Factory {
-	static inline std::unordered_map<IDType, CreateInfo<T, Args ...>> Registry;
+	static inline std::unordered_map<IdType, CreateInfo<T, Args ...>> Registry;
 
 public:
-	Factory() noexcept;
-
-	struct TypeID
-	{ IDType id; };
-
 	struct Exception : std::runtime_error
 	{ using std::runtime_error::runtime_error; };
 
-	template <typename DT, IDType typeID>
-	class Registrar {
-		static const inline bool IsRegistered = [] {
-			return Registry.try_emplace(typeID,
-					[] (void* ptr, Args&& ... args) -> T*
-					{ return ::new(ptr) DT(std::forward<Args>(args) ...); },
-					sizeof(DT)
-			).second;
+	template <typename DT, IdType id>
+	struct Registrar {
+		static inline const IdType TYPE = []() {
+			if (auto [pair, inserted] = Factory::Registry.try_emplace(id,
+				[] (void* ptr, Args&& ... args) -> T* { return ::new(ptr) DT(std::forward<Args>(args) ...); }, sizeof(DT)); inserted)
+				return pair->first;
+			throw Exception("Registered Id");
 		}();
-
 	protected:
-		static constexpr TypeID TypeId {typeID};
-
 		__attribute__((__used__))
-		Registrar() noexcept;
-	};//class Registrar<DT, IDType>
+		Registrar();
+	};//struct Registrar<DT, IdType>
 
 	static CreateInfo<T, Args ...> const&
-	GetCreateInfo(TypeID const& TypeId);
+	GetCreateInfo(IdType const& typeId);
 
 	static T*
-	Create(TypeID const& TypeId, Args&& ... args);
-
-	virtual TypeID
-	getTypeId() = 0;
-};//class Factory<T, IDType, Args ...>
+	Create(IdType const& typeId, Args&& ... args);
+};
+//class Factory<T, IdType, Args ...>
 
 }//namespace DP
 
-#include "../src/Factory.hpp"
+#include "../../src/Factory.hpp"
 
 #endif //DP_FACTORY_H
--- src/Factory.hpp
+++ src/Factory.hpp
@@ -2,30 +2,26 @@
 
 namespace DP {
 
-template <typename T, typename IDType, typename ... Args>
-Factory<T, IDType, Args ...>::Factory() noexcept
-{ Factory::Registry; }
-
-template <typename T, typename IDType, typename ... Args>
+template <typename T, typename IdType, typename ... Args>
 CreateInfo<T, Args ...> const&
-Factory<T, IDType, Args ...>::GetCreateInfo(TypeID const& TypeId)
+Factory<T, IdType, Args ...>::GetCreateInfo(IdType const& typeId)
 {
-	if (auto i = Factory::Registry.find(TypeId.id); i != Factory::Registry.end())
+	if (auto i = Factory::Registry.find(typeId); i != Factory::Registry.end())
 		return i->second;
-	throw Exception("Unregistered Class");
+	throw Exception("Unregistered Id");
 }
 
-template <typename T, typename IDType, typename ... Args>
+template <typename T, typename IdType, typename ... Args>
 T*
-Factory<T, IDType, Args ...>::Create(TypeID const& TypeId, Args&& ... args)
+Factory<T, IdType, Args ...>::Create(IdType const& typeId, Args&& ... args)
 {
-	CreateInfo<T, Args ...> const& createInfo = Factory::GetCreateInfo(TypeId);
+	CreateInfo<T, Args ...> const& createInfo = Factory::GetCreateInfo(typeId);
 	return createInfo.create(::operator new(createInfo.size), std::forward<Args>(args) ...);
 }
 
-template <typename T, typename IDType, typename ... Args>
-template <typename DT, IDType typeID>
-Factory<T, IDType, Args ...>::Registrar<DT, typeID>::Registrar() noexcept
-{ Registrar::IsRegistered; }
+template <typename T, typename IdType, typename... Args>
+template <typename DT, IdType id>
+Factory<T, IdType, Args...>::Registrar<DT, id>::Registrar()
+{ TYPE; }
 
 }//namespace DP
