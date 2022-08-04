7f30e9160965dd7093ac89b3f7f2c022be88632f
ahfakt <a.farukakts@outlook.com>
2022-08-04 23:04:49 +0300

    .

--- include/DP/Builder.hpp
+++ include/DP/Builder.hpp
@@ -7,14 +7,14 @@ namespace DP {
 
 template <std::size_t t, typename T, typename ... U>
 struct TypeImpl
-{ using type = TypeImpl<t - 1, U ...>::type; };
+{ using type = typename TypeImpl<t - 1, U ...>::type; };
 
 template <typename T, typename ... U>
 struct TypeImpl<0, T, U ...>
 { using type = T; };
 
 template <std::size_t t, typename ... T>
-using Type = TypeImpl<t, T ...>::type;
+using Type = typename TypeImpl<t, T ...>::type;
 
 
 template <std::size_t v, auto t, auto ... u>
@@ -38,7 +38,7 @@ struct MemberTypeImpl<M T::*>
 { using type = M; };
 
 template <std::size_t m, auto ... M>
-using MemberType = MemberTypeImpl<Type<m, decltype(M) ...>>::type;
+using MemberType = typename MemberTypeImpl<Type<m, decltype(M) ...>>::type;
 
 
 template <typename P, auto ... M>
@@ -69,7 +69,7 @@ public:
 	T
 	operator()() const
 	{ return build(); }
-};//class DP::Builder<T, M ...>
+};//class DP::Builder<T>
 
 }//namespace DP
 
